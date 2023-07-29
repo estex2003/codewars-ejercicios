edb4a16c7bccea895ec93a1cd06f2a93f5fd6711
Esteban <estebanvelascoriano@gmail.com>
2023-07-28 20:16:10 -0500

    se arreglo un if por un else  y se agrego una nueva condicion

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,10 +1,11 @@
 function evenOrOdd (number) {
     if(number%2 === 1){  //un numero dividido entre 2 si te da 1 es impar, si te da cero es par 
         console.log(number, "es impar")
-    }if(){
+    }if(number%2===0){
         console.log("es par")
-
+        
+    }if(number<0){
+        console.log("escoge un numero positivo plox")
     }
-    
 }
 evenOrOdd() //ingresa el numero en el parentesis 
