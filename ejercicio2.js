e9fefc7a7211160948aae6e378806a01e2631d95
Esteban <estebanvelascoriano@gmail.com>
2023-07-28 19:43:33 -0500

    se agrega el ejercicio 2 que he hecho, funcional

---
+++ ejercicio2.js
@@ -0,0 +1,10 @@
+function evenOrOdd (number) {
+    if(number%2 === 1){  //un numero dividido entre 2 si te da 1 es impar, si te da cero es par 
+        console.log(number, "es impar")
+    }if(){
+        console.log("es par")
+
+    }
+    
+}
+evenOrOdd() //ingresa el numero en el parentesis 
